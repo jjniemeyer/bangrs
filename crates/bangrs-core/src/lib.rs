3092b30af77c097c4e55ccdd6a75b8719b2efd74
Jasper Niemeyer <jani5714@colorado.edu>
2026-04-19 13:05:20 -0600

    red: bangrs-core Cargo.toml + lib.rs

--- crates/bangrs-core/src/lib.rs
+++ crates/bangrs-core/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod error;
+pub mod event;
+pub mod library;
+pub mod player;
+pub mod view_model;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use error::CoreError;
+pub use event::Event;
+pub use library::{AlbumId, Library, Track, TrackId, TrackRow};
+pub use player::Player;
+pub use view_model::ViewModel;
